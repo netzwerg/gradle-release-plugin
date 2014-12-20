cd6212716b5367884604bf0dc05f25bb3fab8134
Rahel Lüthy <rahel.luethy@gmail.com>
2014-12-20 07:05:18 +0100

    Close #7: Task 'release' should depend on 'build' by default

--- src/main/groovy/ch/netzwerg/gradle/release/ReleaseExtension.groovy
+++ src/main/groovy/ch/netzwerg/gradle/release/ReleaseExtension.groovy
@@ -1,11 +1,27 @@
+/**
+ * Copyright 2014 Rahel Lüthy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ch.netzwerg.gradle.release
 
 class ReleaseExtension {
 
+    private static final DEFAULT_DEPENDS_ON = Collections.singletonList('build')
     private static final DEFAULT_PUSH = false
     private static final DEFAULT_SUFFIX = '-SNAPSHOT'
 
-    List<Object> dependsOn = Collections.emptyList()
+    List<Object> dependsOn = DEFAULT_DEPENDS_ON
     boolean push = DEFAULT_PUSH
     String suffix = DEFAULT_SUFFIX
 
